580e7074ef675ae408fddb157e94a5ca13ef9c98
jackeyyang <jackeyyang@coderedfour.com>
2016-08-30 07:45:23 +0800

    delete again start

--- assets/common/js/test/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jackey on 2015/7/23.
- */
-
-$(function(){
-    $(".side-left").load("side-menu.html");
-    $(".sys-menu").load("menu.html");
-});
--- tmod/exam/index.tpl
+++
@@ -1,1 +0,0 @@
-<div>asdfasf</div>
